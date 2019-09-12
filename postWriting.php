6c8081568e62817aaa4a9aea77f161d227681ff9
ilya5006 <samarcev.ilya@mail.ru>
2019-09-12 21:27:28 +0300

    PostWriting functional

--- postWriting.php
+++ postWriting.php
@@ -1,8 +1,21 @@
 <?
+    require_once './php/connection.php';
+
     session_start();
+
     if (!$_SESSION['isLogin'])
     {
         header('Location: index.html');
+    }
+
+    if (isset($_POST['post_send']))
+    {
+        $postTitle = $_POST['post_title'];
+        $postTags = $_POST['post_tags'];
+        $postText = $_POST['post_text'];
+        $query = "INSERT INTO posts VALUES (NULL, '$postTitle', '$postTags', '$postText')";
+        mysqli_query($link, $query);
+        echo '<p>Пост отправлен</p>';
     }
 ?>
 
@@ -11,13 +24,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/postWriting.css">
     <title>Написать пост</title>
 </head>
 <body>
-    <form>
-        <input type="input" name="post_name" placeholder="Заголовок поста">
+    <a href="./admin.php">Вернуться в админку</a>
+    <form action="" method="POST">
+        <input type="input" name="post_title" placeholder="Заголовок поста">
         <input type="input" name="post_tags" placeholder="Теги поста">
-        <textarea name="post_text" placeholder="Текст поста">
+        <textarea name="post_text" placeholder="Текст поста"></textarea>
+        <input type="submit" name="post_send" value="Послать пост">
     </form>
 </body>
 </html>
