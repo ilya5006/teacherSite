4f7077827ac0ddc7a39f008e54589b610d4c8f77
ilya5006 <samarcev.ilya@mail.ru>
2019-09-13 12:42:52 +0300

    Admin panel style

--- postWriting.php
+++ postWriting.php
@@ -16,12 +16,13 @@
     <title>Написать пост</title>
 </head>
 <body>
-    <a href="./admin.php">Вернуться в админку</a>
+    
     <form action="./php/sendPost.php" method="POST">
         <input type="input" name="post_title" placeholder="Заголовок поста" required>
         <input type="input" name="post_tags" placeholder="Теги поста" required>
         <textarea name="post_text" placeholder="Текст поста" required></textarea>
         <input type="submit" name="post_send" value="Послать пост">
     </form>
+    <a href="./admin.php">Вернуться в админку</a>
 </body>
 </html>
