6a5b7b95f18105dbfdfdd502eaa0824bac3dc20b
Alexander <mrreads@yandex.com>
2019-09-15 15:33:01 +0300

    fix input type

--- postWriting.php
+++ postWriting.php
@@ -18,8 +18,8 @@
 <body>
     
     <form action="./php/sendPost.php" method="POST">
-        <input type="input" name="post_title" placeholder="Заголовок поста" required>
-        <input type="input" name="post_tags" placeholder="Теги поста" required>
+        <input type="text" name="post_title" placeholder="Заголовок поста" required>
+        <input type="text" name="post_tags" placeholder="Теги поста" required>
         <textarea name="post_text" placeholder="Текст поста" required></textarea>
         <input type="submit" name="post_send" value="Послать пост">
     </form>
